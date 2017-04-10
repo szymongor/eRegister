7a83a71e7626c41759a9244ac72cf6f2f2a94a27
KrygTomasz <yaami93@gmail.com>
2017-04-10 21:25:56 +0200

    iOS app with Alamofire framework

---
+++ iOS/eRegistry/Pods/Target Support Files/Pods-eRegistryTests/Pods-eRegistryTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_eRegistryTests : NSObject
+@end
+@implementation PodsDummy_Pods_eRegistryTests
+@end
---
+++ iOS/eRegistry/eRegistry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eRegistry
+//
+//  Created by Kryg Tomasz on 10.04.2017.
+//  Copyright © 2017 Kryg Tomek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
