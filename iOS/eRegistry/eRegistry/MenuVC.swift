fe5e0006a7167bb68d2bc17753fe7ac4989b8096
Kryg Tomasz <kryguu@MacBook-Air.local>
2017-04-11 21:33:48 +0200

    Menu ViewController after log in

--- iOS/eRegistry/eRegistry/MenuVC.swift
+++ iOS/eRegistry/eRegistry/MenuVC.swift
@@ -2,7 +2,7 @@
 //  MenuVC.swift
 //  eRegistry
 //
-//  Created by Kryg Tomasz on 10.04.2017.
+//  Created by Kryg Tomasz on 11.04.2017.
 //  Copyright © 2017 Kryg Tomek. All rights reserved.
 //
 
@@ -10,9 +10,20 @@ import UIKit
 
 class MenuVC: UIViewController {
 
+//    @IBOutlet weak var collectionView: UICollectionView! {
+//        didSet {
+//            let cellNib = UINib(nibName: "MenuCVCell", bundle: nil)
+//            collectionView.register(cellNib, forCellWithReuseIdentifier: "MenuCVCell")
+//            collectionView.delegate = self
+//            collectionView.dataSource = self
+//        }
+//    }
+    
+    //let NUMBER_OF_ROWS: CGFloat = 3
+    //let NUMBER_OF_COLUMNS: CGFloat = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationController?.isNavigationBarHidden = false
 
         // Do any additional setup after loading the view.
     }
@@ -21,16 +32,38 @@ class MenuVC: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+//extension MenuVC: UICollectionViewDelegate, UICollectionViewDataSource {
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCVCell", for: indexPath)
+//        cell.backgroundColor = UIColor.red
+//        return cell
+//    }
+//    
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 6
+//    }
+//    
+//}
+//
+//extension MenuVC: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        var cellEdge = 0
+//        let cellWidth = collectionView.bounds.width/NUMBER_OF_COLUMNS
+//        let cellHeight = collectionView.bounds.height/NUMBER_OF_ROWS
+//        cellEdge = Int(cellWidth < cellHeight ? cellWidth : cellHeight)
+//        return CGSize(width: cellEdge, height: cellEdge)
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsetsMake(0, 0, 0, 0)
+//    }
+//
+//}
